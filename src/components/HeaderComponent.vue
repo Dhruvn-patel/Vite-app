b949cb6009db6fa4e9e37df44fe91290b2c7deef
Dhruvn-patel <dhruvpateldhruvpatel01669@gmail.com>
2023-12-20 23:27:07 +0530

    header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -9,22 +9,18 @@
   <nav>
     <div
       class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+      
     >
-      <a
-        href="https://flowbite.com/"
-        class="flex items-center space-x-3 rtl:space-x-reverse"
-      >
-        <span
-          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-          >DN</span
-        >
+      <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <span class="self-center text-2xl">DN</span>
       </a>
       <button
         data-collapse-toggle="navbar-default"
         type="button"
-        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden focus:outline-none focus:ring-2"
+        :class="{'hidden':isToogle}"
         aria-controls="navbar-default"
-        aria-expanded="false"
+        @click="isToogle = !isToogle"
       >
         <span class="sr-only">Open main menu</span>
         <svg
@@ -43,14 +39,24 @@
           />
         </svg>
       </button>
+
+      <button
+        data-collapse-toggle="navbar-default"
+        type="button"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden focus:outline-none focus:ring-2"
+        :class="{'hidden':!isToogle}"
+        @click="isToogle = !isToogle"
+      >
+        X
+      </button>
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul
-          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+          class="font-medium flex flex-col p-4 md:p-0 mt-4 border md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md: dark: md:dark: dark:"
         >
           <li>
             <a
               href="#"
-              class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+              class="block py-2 px-3 rounded md:bg-transparent md: md:p-0 dark: md:dark:"
               aria-current="page"
               >Home</a
             >
@@ -58,28 +64,74 @@
           <li>
             <a
               href="#"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              class="block py-2 px-3 rounded md:bg-transparent md: md:p-0 dark: md:dark:"
               >About</a
             >
           </li>
           <li>
             <a
               href="#"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >Services</a
+              class="block py-2 px-3 rounded md:bg-transparent md: md:p-0 dark: md:dark:"
+              >Skills</a
             >
           </li>
           <li>
             <a
               href="#"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >Pricing</a
+              class="block py-2 px-3 rounded md:bg-transparent md: md:p-0 dark: md:dark:"
+              >Projects</a
             >
           </li>
           <li>
             <a
               href="#"
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              class="block py-2 px-3 rounded md:bg-transparent md: md:p-0 dark: md:dark:"
+              >Contact</a
+            >
+          </li>
+        </ul>
+      </div>
+
+      <div
+        class="md:hidden md:w-auto md:h-full absolute top-[50px]"
+        :class="{ hidden: !isToogle }"
+      >
+        <ul
+          class="font-medium flex flex-col items-center  gap-4 p-4 md:p-0 mt-4  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0"
+        >
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-3 rounded md:bg-transparent md: md:p-0 dark: md:dark:"
+              aria-current="page"
+              >Home</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-3 rounded md:bg-transparent md: md:p-0 dark: md:dark:"
+              >About</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-3 rounded md:bg-transparent md: md:p-0 dark: md:dark:"
+              >Skills</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-3 rounded md:bg-transparent md: md:p-0 dark: md:dark:"
+              >Projects</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-3 rounded md:bg-transparent md: md:p-0 dark: md:dark:"
               >Contact</a
             >
           </li>
@@ -91,6 +143,11 @@
 <script>
 export default {
   name: "HeaderComponent",
+  data() {
+    return {
+      isToogle: false,
+    };
+  },
 };
 </script>
 <style scoped></style>
